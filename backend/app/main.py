d14288e16336cbc9d6c6949d22752370a5b358ae
derrabbyt <florian.rammerstorfer1@gmail.com>
2026-07-25 14:48:38 +0200

    Add routing

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,22 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 
-app = FastAPI()
+app = FastAPI(
+    docs_url="/api/docs",
+    openapi_url="/api/openapi.json",
+    redoc_url="/api/redoc",
+)
 
-@app.get("/")
+api = APIRouter(prefix="/api")
+
+@api.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/health")
+@api.get("/health")
 async def health():
     return {"message": "Service is running"}
 
-@app.post("/items/")
+@api.post("/items/")
 async def create_item(item: dict):
     print(f"Received item: {item}")
     return {"item": item["brand"]}
 
-@app.get("/item/{item_id}")
+@api.get("/item/{item_id}")
 async def get_item(item_id: int):
     return {"should get item for id: ": item_id}
 
-
+app.include_router(api)
