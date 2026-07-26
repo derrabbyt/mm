30bd6dc0e5576708ee74385e29affd00b6f50876
derrabbyt <florian.rammerstorfer1@gmail.com>
2026-07-26 14:41:49 +0200

    Add sample maplibre map

--- frontend/src/app/components/map/map.ts
+++ frontend/src/app/components/map/map.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {MapComponent} from '@maplibre/ngx-maplibre-gl';
 
 @Component({
   selector: 'app-map',
-  imports: [],
+  standalone: true,
+  imports: [MapComponent],
   templateUrl: './map.html',
   styleUrl: './map.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+
 })
-export class Map {}
+export class Map {
+
+  readonly mapStyle = 'https://tiles.openfreemap.org/styles/liberty';
+
+  // IMPORTANT: MapLibre coordinates are [longitude, latitude]
+  readonly center: [number, number] = [16.3738, 48.2082];
+
+  readonly zoom: [number] = [12];
+}
+
