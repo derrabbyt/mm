d8ec4aaa84e231769bbb98cba6275b8733aa5deb
derrabbyt <florian.rammerstorfer1@gmail.com>
2026-07-26 14:31:23 +0200

    Install maplibre package and add blank /map page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Test } from './components/test/test';
+import { Map } from './components/map/map';
 
 export const routes: Routes = [
-    { path: 'test', component: Test }
+    { path: 'test', component: Test },
+    {path: 'map', component: Map},
 ];
 
 
