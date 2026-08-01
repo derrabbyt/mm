cfdabf570e6b40366fa8776e20292f0205937511
derrabbyt <florian.rammerstorfer1@gmail.com>
2026-08-01 09:37:16 +0200

    chore(api): regenerate client rom OpenAPI

--- frontend/src/app/open-api/api/api.ts
+++ frontend/src/app/open-api/api/api.ts
@@ -1,5 +1,7 @@
+export * from './accounts.service';
+import { AccountsService } from './accounts.service';
 export * from './main-test-api.service';
 import { MainTestApiService } from './main-test-api.service';
 export * from './meetup.service';
 import { MeetupService } from './meetup.service';
-export const APIS = [MainTestApiService, MeetupService];
+export const APIS = [AccountsService, MainTestApiService, MeetupService];
